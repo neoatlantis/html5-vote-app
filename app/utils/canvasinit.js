a51f52d990a5e15f5ba0cc0582fd5b04e81033ac
NeoAtlantis <contact@chaobai.li>
2022-09-28 22:26:23 +0200

    enabled smoothing

--- app/utils/canvasinit.js
+++ app/utils/canvasinit.js
@@ -40,7 +40,7 @@ function setup_canvas(canvas, width, height) {
     //ctx.scale(scaleFactor, scaleFactor);
 
     const ctx = canvas.getContext("2d");
-    ctx.imageSmoothingEnabled = false;
+    //ctx.imageSmoothingEnabled = false;
 
     bind_canvas_event_emitters(canvas);
 }
