b06bc612ca569c6fe8888bfeece4c2f6b979dd19
NeoAtlantis <contact@chaobai.li>
2022-09-28 21:40:23 +0200

    audio fix

---
+++ app/audio-controller.js
@@ -0,0 +1,44 @@
+class AudioController {
+    
+    constructor(audio, src, duration){
+        this.src = src;
+        this.duration = duration;
+        this.audio = audio;
+        this.bind_events();
+
+        this.reloaded = false;
+    }
+
+    bind_events(){
+        this.audio.oncomplete = ()=>{
+            this.reloaded = false;
+            this.reload();
+        }
+
+
+        this.audio.oncanplaythrough = () => {
+            this.reloaded = true;
+        }
+
+        this.audio.ontimeupdate = ()=>{
+            if(this.audio.currentTime > this.duration){
+                this.audio.pause();
+                console.log(this.audio.currentTime);
+                this.audio.currentTime = 0;
+            }
+        }
+    }
+
+    reload(){
+        if(this.reloaded) return;
+        this.audio.src = this.src;
+    }
+
+    play(){
+        this.reload();
+        this.audio.play();
+    }
+
+}
+
+export default AudioController;
--- app/vue/touch-audio.vue
+++ app/vue/touch-audio.vue
@@ -13,47 +13,40 @@
 
 <script>
 
-function mocked_factory(url){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            return resolve(url);
-        }, 200)
-    });
-}
+import AudioController from "app/audio-controller";
+
+let audio_button_controller = null;
+let audio_icon_controller = null;
 
 export default {
+
+    mounted(){
+        audio_button_controller = new AudioController(
+            document.getElementById("touchbuttonaudio"),
+            "./data/touch-button.mp3",
+            0.4
+        );
+        
+        audio_icon_controller = new AudioController(
+            document.getElementById("touchiconaudio"),
+            "./data/touch-icon.mp3",
+            0.2
+        );
+    },
+
     data(){ return {
         touch_button_ogg: "./data/touch-button.ogg",
         touch_icon_ogg: "./data/touch-icon.ogg",
         touch_button_mp3: "./data/touch-button.mp3",
         touch_icon_mp3: "./data/touch-icon.mp3",
-        firsttime: true,
     } },
 
     methods: {
         play_button(){
-            /*if(!this.firsttime){
-                document.getElementById("touchbuttonaudio").play();
-                return;
-            }*/
-            mocked_factory(this.touch_button_mp3).then(src => {
-                const audio = document.getElementById("touchbuttonaudio");
-                audio.src = src;
-                audio.play();
-                this.firsttime = false;
-            });
+            audio_button_controller.play();
         },
         play_icon(){
-            /*if(!this.firsttime){
-                document.getElementById("touchiconaudio").play();
-                return;
-            }*/
-            mocked_factory(this.touch_icon_mp3).then(src => {
-                const audio = document.getElementById("touchiconaudio");
-                audio.src = src
-                audio.play()
-                this.firsttime = false;
-            });
+            audio_icon_controller.play();
         }
     }
 }
